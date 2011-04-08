c91144332a30e71cbb1f887d5928a581b097520f
Pandu Poluan <pandu@poluan.info>
2011-04-08 13:12:03 +0700

    Add "version" command, insert $version in "help" output

--- opt/wallmator/bin/wallmator.sh
+++ opt/wallmator/bin/wallmator.sh
@@ -2,9 +2,14 @@
 
 source /opt/wallmator/include/CONSTANTS.sh
 
+Show_Version () {
+  printf "\n%s\n" "\
+WallMator $WALLMATOR_version"
+}
+
 Show_Help () {
   printf "\n%b\n" "\
-${w}wallmator$n - ${c}fireWALL autoMATOR$n
+${w}WallMator$n $WALLMATOR_version - ${c}fireWALL autoMATOR$n
 
 Syntax:
     $w$0$n $c<command>$n
@@ -16,6 +21,9 @@ $c<command>$n can be:
 
     ${w}save$n
         Saves the current firewall state
+        
+    ${w}version$n
+        Prints the version of wallmator
 
     ${w}help$n
         Prints this help
@@ -32,6 +40,9 @@ case $command in
     ;;
   save)
     chainto="Save.sh"
+    ;;
+  version)
+    Show_Version
     ;;
   help)
     Show_Help
