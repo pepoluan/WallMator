a9e8d07b67474af158d69a9de7889ef8643bc0ce
Pandu Poluan <pandu@poluan.info>
2011-04-08 13:12:51 +0700

    Add version var Move programs alias-vars here Add SafeSource function

--- opt/wallmator/include/CONSTANTS.sh
+++ opt/wallmator/include/CONSTANTS.sh
@@ -5,6 +5,8 @@
 
 # When sourced, this script will set all common variables
 
+WALLMATOR_version="0.1.0"
+
 c="\x1B[1;36m"
 g="\x1B[1;32m"
 r="\x1B[1;41m"
@@ -18,6 +20,30 @@ bindir="/opt/wallmator/bin"
 etcdir="/etc/opt/wallmator"
 incdir="/opt/wallmator/include"
 
+# Location of programs
+
+chmod="/bin/chmod"
+date="/bin/date"
+mktmp="/bin/mktemp"
+awk="/usr/bin/awk"
+grep="/bin/grep"
+rm="/bin/rm"
+
+ip="/sbin/ip"
+ipset="/usr/local/sbin/ipset"
+iptrest="/sbin/iptables-restore"
+
+SafeSource () {
+  __temp=$($mktemp)
+  # Get only actual parameters ...
+  $grep -E '^[[:space:]]*[a-z0-9_]+=' "$1" |
+    # ... and strip out embedded commands ...
+    $grep -v -e '`' -e '$(' >
+      # ... and stow it somewhere temporary
+      $__temp
+  source $__temp
+  $rm -f $__temp
+}
+
 ## THIS FILE IS SOURCED!!
 ## DO NOT END WITH exit !!!
-
